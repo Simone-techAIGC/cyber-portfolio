aee3c6e72cfda4382f62b48481981a0a0d9dea50
Simone <sagefoster07simone@gmail.com>
2026-05-24 03:44:16 +0800

    Update vite.config.js
    edit config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/cyber-portfolio/',
   plugins: [react(), tailwindcss()],
-  base: '/cyber-portfolio/',  // 👈 必须写，仓库名
 })
